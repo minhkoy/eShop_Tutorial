db7d6bb9040fcd7cb6cc087b4425f231164a996d
minh <pikoycr2001@gmail.com>
2025-04-08 00:00:40 +0700

    add docker support

--- Catalog.Api/Products/CreateProducts/CreateProductHandler.cs
+++ Catalog.Api/Products/CreateProducts/CreateProductHandler.cs
@@ -1,15 +1,26 @@
-﻿using BuildingBlocks.CQRS;
-using MediatR;
+﻿
 
 namespace Catalog.Api.Products.CreateProducts
 {
     public record CreateProductCommand(string Name, List<string> Categories, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    public class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var product = new Product
+            {
+                Name = request.Name,
+                Categories = request.Categories,
+                Description = request.Description,
+                ImageFile = request.ImageFile,
+                Price = request.Price,
+            };
+
+            session.Store(product);
+            await session.SaveChangesAsync();
+            //TODO: Save to database and return the result
+            return new CreateProductResult(product.Id);
         }
     }
 
--- Catalog.Api/Program.cs
+++ Catalog.Api/Program.cs
@@ -7,10 +7,16 @@ builder.Services.AddMediatR(config =>
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 
 });
+builder.Services.AddMarten(o =>
+{
+    o.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
 
 var app = builder.Build();
 
-app.MapDefaultEndpoints();
+
+//app.MapDefaultEndpoints();
 
 app.MapCarter();
+
 app.Run();
