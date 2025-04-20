97b749b6640a5fd47187014cb9a62965bd7e5b69
minh <pikoycr2001@gmail.com>
2025-04-21 00:29:48 +0700

    get products endpoint

---
+++ Catalog.Api/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace Catalog.Api.Products.GetProducts
+{
+    public record GetProductsQuery : IQuery<GetProductsResult>;
+
+    public record GetProductsResult(IEnumerable<Product> Products);
+
+    internal class GetProductsHandler(IDocumentSession session, ILogger<GetProductsHandler> logger)
+        : IQueryHandler<GetProductsQuery, GetProductsResult>
+    {
+        public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductsQueryHandler.Handle is called with {@Query}", query);
+            var products = await session.Query<Product>().ToListAsync();
+            return new GetProductsResult(products);
+        }
+    }
+}
