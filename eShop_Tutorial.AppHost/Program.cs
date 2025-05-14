8932d9c5a4e02ab79f30c46212cdbeb28c774a96
minh <pikoycr2001@gmail.com>
2025-05-14 23:55:25 +0700

    Lec 103: Init Basket microservice

--- eShop_Tutorial.AppHost/Program.cs
+++ eShop_Tutorial.AppHost/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.Catalog_Api>("catalog-api");
 
+builder.AddProject<Projects.Basket_Api>("basket-api");
+
 builder.Build().Run();
